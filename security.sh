e72432d96a1e4a345d7a57bc902540dd347eaf9c
CowCat <114646151+CowCat271@users.noreply.github.com>
2026-06-21 06:40:00 +0300

    Change single brackets to double brackets in conditions

--- security.sh
+++ security.sh
@@ -21,7 +21,7 @@ describe_ec2_key(){
 create_ec2_key(){
     echo "Start create_ec2_key ..."
     aws ec2 create-key-pair --key-name $key_name --key-type ed25519 --key-format $key_format --query 'KeyMaterial' --output text > "${key_name}.${key_format}"
-    if ! [ -f "${key_name}.${key_format}" ]; then
+    if [[ ! -f "${key_name}.${key_format}" ]]; then
         echo "ERROR: couldn't create the ec2 key."
         exit 1
     else
@@ -51,7 +51,7 @@ create_secret(){
 
     secret_arn=$(aws secretsmanager create-secret --name $key_name --description "EC2 ssh key" --secret-string file://"${key_name}.${key_format}" | grep -oP "(?<=\"ARN\": \")[^\"]*")
     
-    if [ "$secret_arn" == "" ]; then
+    if [[ "$secret_arn" == "" ]]; then
         echo "ERROR: couldn't create the secret."
         exit 1
     else
@@ -76,7 +76,7 @@ create_elb_sg(){
     elb_sg_id=$(aws ec2 describe-security-groups \
             --filters Name=group-name,Values=${elb_sg_name} | grep -oP '(?<="GroupId": ")[^"]*' | uniq)
 
-    if [ "$elb_sg_id" == "" ]; then
+    if [[ "$elb_sg_id" == "" ]]; then
         
         echo "ELB Security Group will be created"
 
@@ -86,7 +86,7 @@ create_elb_sg(){
         echo $elb_sg_id
 
         elb_sg_id=$(echo "$elb_sg_id"| grep -oP '(?<="GroupId": ")[^"]*' | uniq)
-        if [ "$elb_sg_id" == "" ]; then
+        if [[ "$elb_sg_id" == "" ]]; then
             echo "ERROR: couldn't create the ELB security group."
             exit 1
         fi
@@ -97,7 +97,7 @@ create_elb_sg(){
             echo "Adding elb rule: $elb_rule"
             ADD_RULE_OUTPUT=$(aws ec2 authorize-security-group-ingress --group-id $elb_sg_id --ip-permissions "$elb_rule" 2>&1)
 
-            if [ $? -ne 0 ]; then
+            if [[ $? -ne 0 ]]; then
                 echo "ERROR: adding elb rule '$elb_rule': $ADD_RULE_OUTPUT"
                 echo "deleting the elb security group ..."
                 aws ec2 delete-security-group --group-id $elb_sg_id
@@ -119,7 +119,7 @@ create_ec2_sg(){
     ec2_sg_id=$(aws ec2 describe-security-groups \
             --filters Name=group-name,Values=${ec2_sg_name} | grep -oP '(?<="GroupId": ")[^"]*' | uniq)
 
-    if [ "$ec2_sg_id" == "" ]; then
+    if [[ "$ec2_sg_id" == "" ]]; then
         
         echo "EC2 Security Group will be created"
 
@@ -129,7 +129,7 @@ create_ec2_sg(){
         echo $ec2_sg_id
 
         ec2_sg_id=$(echo "$ec2_sg_id"| grep -oP '(?<="GroupId": ")[^"]*' | uniq)
-        if [ "$ec2_sg_id" == "" ]; then
+        if [[ "$ec2_sg_id" == "" ]]; then
             echo "ERROR: couldn't create the ec2 security group."
             exit 1
         fi
@@ -143,7 +143,7 @@ create_ec2_sg(){
             echo "Adding ec2 rule: $ec2_rule"
             ADD_RULE_OUTPUT=$(aws ec2 authorize-security-group-ingress --group-id $ec2_sg_id --ip-permissions "$ec2_rule" 2>&1)
 
-            if [ $? -ne 0 ]; then
+            if [[ $? -ne 0 ]]; then
                 echo "ERROR: adding ec2 sg rule '$rule': $ADD_RULE_OUTPUT"
                 echo "deleting the ec2 security group ..."
                 aws ec2 delete-security-group --group-id $ec2_sg_id
@@ -161,14 +161,14 @@ create_ec2_sg(){
 
 # create ec2 kay and store it in a secret
 describe_ec2_key
-if [ "$ec2_key" == "" ]; then
+if [[ "$ec2_key" == "" ]]; then
     echo "key is not exists."
     echo "Create key and secret."
     create_ec2_key
     create_secret
 else
     describe_secret
-    if [ "$secret_arn" == "" ]; then
+    if [[ "$secret_arn" == "" ]]; then
         echo "key exists but secret not exists."
         echo "Deleting everything then create new key and secret."
         delete_ec2_key
